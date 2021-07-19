4b1fb690d5aadd975a030bf8637ffcc3163c9379
Ilkay Yildiz <ilkyyldz95@gmail.com>
2021-07-19 13:01:02 -0700

    read completed reviews. files with no reviews become training data. put labels in a dictionary with key containing patient id and date, values containing a list of label, start, duration in seconds

--- read_eeg_human.py
+++ read_eeg_human.py
@@ -7,10 +7,101 @@ import time
 from mne.io import RawArray, read_raw_edf
 from os import listdir
 from os.path import isfile, join
+from shutil import copyfile
 import matplotlib.pyplot as plt
 import pickle
 from scipy.signal import butter, lfilter, freqz
+import xlrd, datetime
 
+signal_dir = "/ifs/loni/faculty/dduncan/epibios_collab/iyildiz/human_eeg/"
+label_dir = "/ifs/loni/faculty/dduncan/epibios_collab/iyildiz/human_eeg_completed_reviews/"
+
+# Move all reviewed raw .edf files to the desired directory
+if not os.path.isdir(signal_dir):
+    for path, subdirs, files in os.walk("/ifs/loni/faculty/dduncan/persyst_temporary_storage/TO BE REVIEWED - Already Processed"):
+        for name in files:
+            if ".edf" in name:
+                copyfile(os.path.join(path, name), signal_dir + os.path.join(path, name).replace("/","_"))
+
+# Find all unique patient ids
+all_label_file_names = os.listdir(label_dir)
+training_signal_names = []
+ictal_signal_dict = dict()
+
+# Create label dictionary
+# Key format: (3_17_0089)_EEG_2018-12-21
+# Value Format: (label seizure/pd/rda, start time in s, duration in s)
+for label_file_name in all_label_file_names:
+    label_start_idx = 7
+    print("Reading", label_file_name)
+    patient_name = label_file_name[label_file_name.find("3_"):label_file_name.find("3_")+9]
+    print("Patient", patient_name)
+    xl_workbook = xlrd.open_workbook(os.path.join(label_dir, label_file_name))
+    xl_sheet = xl_workbook.sheet_by_name('EpiBioS4Rx EEG Reviewer Form')
+    if xl_sheet.nrows <= label_start_idx:
+        print("No label found for", label_file_name)
+        training_signal_names.append(label_file_name)
+        continue
+    event_date = xl_sheet.cell(label_start_idx, 0).value
+    if not isinstance(event_date, float):
+        print("No label found for", label_file_name)
+        training_signal_names.append(label_file_name)
+        continue
+    else:
+        while isinstance(event_date, float):
+            # read date and time as they are
+            # date: year/month/date -> year-month-day
+            event_date = str(xlrd.xldate.xldate_as_datetime(event_date, xl_workbook.datemode))\
+                                .split(" ")[0]
+            key = "(" + patient_name + ")_EEG_" + event_date
+            # time: minute:second:milisecond -> seconds
+            event_start = xl_sheet.cell(label_start_idx, 1).value
+            event_start = str(xlrd.xldate.xldate_as_datetime(event_start, xl_workbook.datemode))\
+                                .split(" ")[1]
+            event_start_s = float(event_start.split(":")[0]) * 60 + float(event_start.split(":")[1])
+            # Read labels
+            seizure_label = xl_sheet.cell(label_start_idx, 3).value
+            pd_label = xl_sheet.cell(label_start_idx, 12).value
+            rda_label = xl_sheet.cell(label_start_idx, 24).value
+            if isinstance(seizure_label, float) and float(seizure_label) == 1:
+                event_label = "seizure"
+                event_duration = float(xl_sheet.cell(label_start_idx, 10).value)
+                if key in ictal_signal_dict.keys():
+                    ictal_signal_dict[key].append((event_label, event_start_s, event_duration))
+                else:
+                    ictal_signal_dict[key] = [(event_label, event_start_s, event_duration)]
+                print(key, ictal_signal_dict[key])
+            elif isinstance(pd_label, float) and float(pd_label) == 1:
+                event_label = "pd"
+                if not isinstance(xl_sheet.cell(label_start_idx, 20).value, float):
+                    event_duration = float(xl_sheet.cell(label_start_idx, 21).value) * 60  # convert to s
+                else:
+                    event_duration = float(xl_sheet.cell(label_start_idx, 20).value)
+                if key in ictal_signal_dict.keys():
+                    ictal_signal_dict[key].append((event_label, event_start_s, event_duration))
+                else:
+                    ictal_signal_dict[key] = [(event_label, event_start_s, event_duration)]
+                print(key, ictal_signal_dict[key])
+            elif isinstance(rda_label, float) and float(rda_label) == 1:
+                event_label = "rda"
+                if not isinstance(xl_sheet.cell(label_start_idx, 32).value, float):
+                    event_duration = float(xl_sheet.cell(label_start_idx, 33).value) * 60  # convert to s
+                else:
+                    event_duration = float(xl_sheet.cell(label_start_idx, 32).value)
+                if key in ictal_signal_dict.keys():
+                    ictal_signal_dict[key].append((event_label, event_start_s, event_duration))
+                else:
+                    ictal_signal_dict[key] = [(event_label, event_start_s, event_duration)]
+                print(key, ictal_signal_dict[key])
+            else:
+                print("No label found for", label_file_name, event_date, event_start)
+            # move to the next row
+            label_start_idx += 1
+            if label_start_idx >= xl_sheet.nrows:
+                break
+            event_date = xl_sheet.cell(label_start_idx, 0).value
+
+"""
 modality = "rodent_eeg"  # staba and monash
 fs = 2000.0
 lowcut = 0.5
@@ -75,4 +166,4 @@ for signal in all_eegs:
 # Save preprocessed EEG data
 with open('{}_preprocessed_eegs.pickle'.format(modality), 'wb') as handle:
     pickle.dump(prep_eegs, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
+"""
